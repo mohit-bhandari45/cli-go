a0e366d2fa5eac05a7134f44fdf074ba48bfce16
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-14 18:10:01 +0530

    feat(stats): create Summary data structure

---
+++ internal/stats/stats.go
@@ -0,0 +1,23 @@
+package stats
+
+import (
+	"time"
+
+	"github.com/mohit-bhandari45/gurl/internal/runner"
+)
+
+type Summary struct {
+	TotalRequests   int            `json:"total_requests"`
+	SuccessRequests int            `json:"success_requests"`
+	FailedRequests  int            `json:"failed_requests"`
+	TotalDuration   time.Duration  `json:"total_duration_ns"`
+	RPS             float64        `json:"requests_per_second"`
+	MinDuration     time.Duration  `json:"min_duration_ns"`
+	MaxDuration     time.Duration  `json:"max_duration_ns"`
+	AvgDuration     time.Duration  `json:"avg_duration_ns"`
+	P50             time.Duration  `json:"p50_duration_ns"`
+	P95             time.Duration  `json:"p95_duration_ns"`
+	P99             time.Duration  `json:"p99_duration_ns"`
+	StatusCodes     map[int]int    `json:"status_codes"`
+	Errors          map[string]int `json:"errors,omitempty"`
+}
