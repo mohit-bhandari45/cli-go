620244df40719dee957db1f4f63e479482a00e36
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-14 21:35:15 +0530

    feat(stats): add calculatePercentile helper function

--- internal/stats/stats.go
+++ internal/stats/stats.go
@@ -21,3 +21,16 @@ type Summary struct {
 	StatusCodes     map[int]int    `json:"status_codes"`
 	Errors          map[string]int `json:"errors,omitempty"`
 }
+
+func calculatePercentile(durations []time.Duration, p float64) time.Duration {
+	if len(durations) == 0 {
+		return 0;
+	}
+
+	index := int(float64(len(durations)) * p);
+	if index > len(durations) {
+		index = len(durations) - 1;
+	}
+
+	return durations[index];
+}
