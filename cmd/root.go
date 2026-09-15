ab4c110739d55933631a0324d159dfcdd577ea29
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-15 11:19:19 +0530

    root updates

--- cmd/root.go
+++ cmd/root.go
@@ -43,8 +43,7 @@ var rootCmd = &cobra.Command{
 		totalDuration := time.Since(start);
 		summary := stats.Calculate(results, totalDuration);
 
-		fmt.Println(totalDuration, summary);
-		fmt.Println(totalDuration.Seconds(), summary);
+		fmt.Printf("Completed in %v! RPS: %.2f\n", totalDuration, summary.RPS)
 
 		return nil
 	},
